b0e70ab75a2990637e5b4a05d0a2e0980bbc1391
Dyy-Coding <chandyneat9999@gmail.com>
2025-06-23 08:33:29 +0700

    code

--- Back-End/app/Http/Controllers/AuthorController.php
+++ Back-End/app/Http/Controllers/AuthorController.php
@@ -8,56 +8,51 @@ use Illuminate\Http\Request;
 class AuthorController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of all authors.
      */
-
     public function index()
-
     {
-        //
+        // Fetch all authors from the database using the model
         $authors = new AuthorModel();
+
+        // Return JSON response with all authors and success message
         return response()->json([
-            "message" => "get data successful.",
-             "data" => $authors::all()
-        ],200);
+            "message" => "Get data successful.",
+            "data" => $authors::all()
+        ], 200);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Create a new author record.
+     * This handles the creation logic (usually used with POST).
      */
-   public function create(Request $request)
+    public function create(Request $request)
     {
-        //
-        $authors = new AuthorModel();
-
-
-        $authors = AuthorModel::create(
-            [
+        // Create a new author record using request data
+        $authors = AuthorModel::create([
             'name' => $request->name,
             'dateofbirth' => $request->dateofbirth,
             'nationality' => $request->nationality,
             'numberOfWrittenBook' => $request->numberOfWrittenBook
-            ]
-            
-        );
+        ]);
 
-        if (!$authors){
+        // Check if creation failed, return error if yes
+        if (!$authors) {
             return response()->json([
                 "message" => "Create data fail."
-            ],500);
-        } else{
+            ], 500);
+        } else {
+            // Successfully created - return success message with author data
             return response()->json([
-            'message' => 'Author created successful.',
-            'data' => $authors
-           ], 201);
+                'message' => 'Author created successfully.',
+                'data' => $authors
+            ], 201);
         }
-
-                // Return response
-        
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store method is currently unused.
+     * Usually used to store new resource after validation.
      */
     public function store(Request $request)
     {
@@ -65,12 +60,14 @@ class AuthorController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Display a specific author by ID.
      */
     public function show($id)
     {
-        //
+        // Find author by primary key or fail with 404
         $author = AuthorModel::findOrFail($id);
+
+        // Return JSON with author data and success message
         return response()->json([
             "message" => "Get author successful.",
             "data" => $author
@@ -78,7 +75,8 @@ class AuthorController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Edit method currently unused.
+     * Usually to show form for editing resource.
      */
     public function edit(AuthorModel $authorModel)
     {
@@ -86,52 +84,52 @@ class AuthorController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Update an existing author record by ID.
      */
     public function update(Request $request, $id)
     {
-        //
-        //
+        // Find the author to update by ID
         $authors = AuthorModel::find($id);
 
-
-        $authors->update(
-            [
+        // Update author fields from request data
+        $authors->update([
             'name' => $request->name,
             'dateofbirth' => $request->dateofbirth,
             'nationality' => $request->nationality,
             'numberOfWrittenBook' => $request->numberOfWrittenBook
-            ]
-            
-        );
+        ]);
 
-        if (!$authors){
+        // If update failed, return error response
+        if (!$authors) {
             return response()->json([
                 "message" => "Update data fail."
-            ],500);
-        } else{
+            ], 500);
+        } else {
+            // Return success message and updated author data
             return response()->json([
-            'message' => 'Book updated.',
-            'data' => $authors
-           ], 201);
+                'message' => 'Author updated successfully.',
+                'data' => $authors
+            ], 201);
         }
-
-                // Return response
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Delete an author record by ID.
      */
     public function destroy($id)
     {
+        // Find the author by ID
         $author = AuthorModel::find($id);
 
+        // If author not found, return 404 response
         if (!$author) {
             return response()->json(['message' => 'Author not found.'], 404);
         }
 
+        // Delete the author record
         $author->delete();
 
+        // Return success message
         return response()->json([
             'message' => 'Author deleted successfully.',
         ]);
